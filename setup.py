04d169903f56d466e80c4d0893c29f689aea4a20
Renan Peres <renanmogo2@protonmail.com>
2024-07-01 12:49:26 -0400

    .gitignore Update

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='fabric_remote_tools',
-    version='0.1.1',
-    packages=find_packages(),
-    install_requires=['python-dotenv', 'typing', 'pandas', 'polars', 'pyarrow', 'deltalake', 'connectorx', 'pyodbc', 'pymssql', 'duckdb', 'requests', 'azure-identity', 'azure-devops', 'azure-storage-file-datalake']
-)
